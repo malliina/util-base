ce182ee96e49c168debfbb532389ee102f4c5302
Michael Skogberg <malliina123@gmail.com>
2015-05-09 15:44:58 +0300

    0.6.3

--- project/UtilBaseBuild.scala
+++ project/UtilBaseBuild.scala
@@ -9,14 +9,14 @@ object UtilBaseBuild extends Build {
 
   lazy val utilSettings = bintraySettings ++ Seq(
     scalaVersion := "2.11.6",
-    version := "0.6.2",
+    version := "0.6.3",
     gitUserName := "malliina",
     organization := s"com.github.${gitUserName.value}",
     developerName := "Michael Skogberg",
     crossScalaVersions := Seq(scalaVersion.value, "2.10.4"),
     libraryDependencies ++= Seq(
-      "io.reactivex" %% "rxscala" % "0.24.0",
-      "com.typesafe.play" %% "play-json" % "2.3.8",
+      "io.reactivex" %% "rxscala" % "0.24.1",
+      "com.typesafe.play" %% "play-json" % "2.3.9",
       "org.java-websocket" % "Java-WebSocket" % "1.3.0"
     ),
     javacOptions ++= Seq("-source", "1.6", "-target", "1.6"),
