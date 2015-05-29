3e8f9ed8eb6f7be43285a02ec2fb92dab3f73276
malliina <malliina123@gmail.com>
2015-05-29 14:52:32 +0300

    0.7.0 with play-json 2.4.0

--- project/UtilBaseBuild.scala
+++ project/UtilBaseBuild.scala
@@ -9,14 +9,14 @@ object UtilBaseBuild extends Build {
 
   lazy val utilSettings = bintraySettings ++ Seq(
     scalaVersion := "2.11.6",
-    version := "0.6.4",
+    version := "0.7.0",
     gitUserName := "malliina",
     organization := s"com.github.${gitUserName.value}",
     developerName := "Michael Skogberg",
     crossScalaVersions := Seq(scalaVersion.value, "2.10.4"),
     libraryDependencies ++= Seq(
       "io.reactivex" %% "rxscala" % "0.24.1",
-      "com.typesafe.play" %% "play-json" % "2.3.9",
+      "com.typesafe.play" %% "play-json" % "2.4.0",
       "org.java-websocket" % "Java-WebSocket" % "1.3.0"
     ),
     javacOptions ++= Seq("-source", "1.6", "-target", "1.6"),
