1148138c6c1fddec748d186baef69839bcd21930
Michael Skogberg <michael.skogberg@outlook.com>
2015-09-06 18:16:06 +0300

    Update deps

--- project/UtilBaseBuild.scala
+++ project/UtilBaseBuild.scala
@@ -8,15 +8,15 @@ object UtilBaseBuild extends Build {
   lazy val p = SbtProjects.testableProject("util-base").settings(utilSettings: _*)
 
   lazy val utilSettings = bintraySettings ++ Seq(
-    scalaVersion := "2.11.6",
-    version := "0.7.0",
+    scalaVersion := "2.11.7",
+    version := "0.8.0",
     gitUserName := "malliina",
     organization := s"com.github.${gitUserName.value}",
     developerName := "Michael Skogberg",
     crossScalaVersions := Seq(scalaVersion.value, "2.10.4"),
     libraryDependencies ++= Seq(
-      "io.reactivex" %% "rxscala" % "0.24.1",
-      "com.typesafe.play" %% "play-json" % "2.4.0",
+      "io.reactivex" %% "rxscala" % "0.25.0",
+      "com.typesafe.play" %% "play-json" % "2.4.2",
       "org.java-websocket" % "Java-WebSocket" % "1.3.0"
     ),
     javacOptions ++= Seq("-source", "1.6", "-target", "1.6"),
