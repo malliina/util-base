0c347748b6456ad29dc4f19c378562f46f2e374e
Michael Skogberg <michael.skogberg@futurice.com>
2023-07-21 11:22:29 +0300

    Support optional config values

--- config/src/main/scala/com/malliina/config/ConfigReadable.scala
+++ config/src/main/scala/com/malliina/config/ConfigReadable.scala
@@ -2,29 +2,51 @@ package com.malliina.config
 
 import com.malliina.http.FullUrl
 import com.malliina.values.{ErrorMessage, Readable}
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigException}
 
 import java.nio.file.{InvalidPathException, Path, Paths}
 import scala.util.control.NonFatal
 
+sealed abstract class ConfigError(val message: ErrorMessage, inner: Option[Exception])
+  extends Exception(message.message, inner.orNull) {
+  def key: String
+}
+class MissingValue(val key: String) extends ConfigError(ErrorMessage(s"Missing: '$key'."), None)
+class InvalidValue(val key: String, message: ErrorMessage, e: Option[Exception])
+  extends ConfigError(message, e) {
+  def this(key: String, e: Option[Exception]) =
+    this(key, ErrorMessage(s"Failed to read '$key'."), e)
+}
+
 trait ConfigReadable[T] {
-  def read(key: String, c: Config): Either[ErrorMessage, T]
+  def read(key: String, c: Config): Either[ErrorMessage, T] =
+    parse(key, c).left.map(_.message)
+  def parse(key: String, c: Config): Either[ConfigError, T] =
+    parseOpt(key, c).flatMap { opt =>
+      opt.toRight(new MissingValue(key))
+    }
+  def parseOpt(key: String, c: Config): Either[InvalidValue, Option[T]]
   def flatMap[U](f: T => ConfigReadable[U]): ConfigReadable[U] = {
     val parent = this
-    (key: String, c: Config) => parent.read(key, c).flatMap(t => f(t).read(key, c))
+    (key: String, c: Config) =>
+      parent
+        .parseOpt(key, c)
+        .flatMap(opt => opt.map(t => f(t).parseOpt(key, c)).getOrElse(Right(None)))
   }
   def emap[U](f: T => Either[ErrorMessage, U]): ConfigReadable[U] = (key: String, c: Config) =>
-    read(key, c).flatMap(f)
+    read(key, c)
+      .flatMap(t => f(t))
+      .fold(err => Left(new InvalidValue(key, err, None)), u => Right(Option(u)))
   def map[U](f: T => U): ConfigReadable[U] = emap(t => Right(f(t)))
 }
 
 object ConfigReadable {
   implicit val string: ConfigReadable[String] =
-    recovered((key: String, c: Config) => Right(c.getString(key)))
+    recovered((key: String, c: Config) => c.getString(key))
   implicit val url: ConfigReadable[FullUrl] =
     string.emap(s => FullUrl.build(s))
   implicit val int: ConfigReadable[Int] =
-    recovered((key: String, c: Config) => Right(c.getInt(key)))
+    recovered((key: String, c: Config) => c.getInt(key))
   implicit val path: ConfigReadable[Path] = ConfigReadable.string.emap { s =>
     try Right(Paths.get(s))
     catch {
@@ -32,16 +54,17 @@ object ConfigReadable {
     }
   }
   implicit val bool: ConfigReadable[Boolean] =
-    recovered((key: String, c: Config) => Right(c.getBoolean(key)))
+    recovered((key: String, c: Config) => c.getBoolean(key))
   implicit val config: ConfigReadable[Config] =
-    recovered((key: String, c: Config) => Right(c.getConfig(key)))
+    recovered((key: String, c: Config) => c.getConfig(key))
 
-  private def recovered[T](unsafe: ConfigReadable[T]): ConfigReadable[T] =
+  private def recovered[T](unsafe: (String, Config) => T): ConfigReadable[T] =
     (key: String, c: Config) =>
-      try unsafe.read(key, c)
+      try Right(Option(unsafe(key, c)))
       catch {
-        case NonFatal(e) =>
-          Left(ErrorMessage(Option(e.getMessage).getOrElse(s"Failed to read '$key'.")))
+        case _: ConfigException.Missing => Right(None)
+        case e: Exception =>
+          Left(new InvalidValue(key, Option(e)))
       }
 
   implicit def readable[T](implicit r: Readable[T]): ConfigReadable[T] =
