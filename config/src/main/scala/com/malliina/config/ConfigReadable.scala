c34609d227b80d520a60582be2031619331f5f27
Michael Skogberg <michael.skogberg@futurice.com>
2023-07-26 21:52:20 +0300

    Clarify emap

--- config/src/main/scala/com/malliina/config/ConfigReadable.scala
+++ config/src/main/scala/com/malliina/config/ConfigReadable.scala
@@ -25,7 +25,7 @@ trait ConfigReadable[T] {
     parseOpt(key, c).flatMap { opt =>
       opt.toRight(new MissingValue(key))
     }
-  def parseOpt(key: String, c: Config): Either[InvalidValue, Option[T]]
+  def parseOpt(key: String, c: Config): Either[ConfigError, Option[T]]
   def flatMap[U](f: T => ConfigReadable[U]): ConfigReadable[U] = {
     val parent = this
     (key: String, c: Config) =>
@@ -33,21 +33,31 @@ trait ConfigReadable[T] {
         .parseOpt(key, c)
         .flatMap(opt => opt.map(t => f(t).parseOpt(key, c)).getOrElse(Right(None)))
   }
-  def emap[U](f: T => Either[ErrorMessage, U]): ConfigReadable[U] = (key: String, c: Config) =>
-    read(key, c)
-      .flatMap(t => f(t))
-      .fold(err => Left(new InvalidValue(key, err, None)), u => Right(Option(u)))
-  def map[U](f: T => U): ConfigReadable[U] = emap(t => Right(f(t)))
+  def emap[U](f: T => Either[ConfigError, U]): ConfigReadable[U] = flatMap { t => (key, _) =>
+    f(t).map(u => Option(u))
+  }
+  def emapParsed[U](f: T => Either[ErrorMessage, U]): ConfigReadable[U] = flatMap { t => (key, _) =>
+    f(t).map(u => Option(u)).left.map(err => new InvalidValue(key, err, None))
+  }
+  def map[U](f: T => U): ConfigReadable[U] = emapParsed(t => Right(f(t)))
 }
 
 object ConfigReadable {
+  implicit class ConfigOps(val c: Config) extends AnyVal {
+    def read[T](key: String)(implicit r: ConfigReadable[T]): Either[ErrorMessage, T] =
+      r.read(key, c)
+    def parse[T](key: String)(implicit r: ConfigReadable[T]): Either[ConfigError, T] =
+      r.parse(key, c)
+    def opt[T](key: String)(implicit r: ConfigReadable[T]): Either[ConfigError, Option[T]] =
+      r.parseOpt(key, c)
+  }
   implicit val string: ConfigReadable[String] =
     recovered((key: String, c: Config) => c.getString(key))
   implicit val url: ConfigReadable[FullUrl] =
-    string.emap(s => FullUrl.build(s))
+    string.emapParsed(s => FullUrl.build(s))
   implicit val int: ConfigReadable[Int] =
     recovered((key: String, c: Config) => c.getInt(key))
-  implicit val path: ConfigReadable[Path] = ConfigReadable.string.emap { s =>
+  implicit val path: ConfigReadable[Path] = ConfigReadable.string.emapParsed { s =>
     try Right(Paths.get(s))
     catch {
       case ipe: InvalidPathException => Left(ErrorMessage(s"Invalid path: '$s'."))
@@ -68,5 +78,5 @@ object ConfigReadable {
       }
 
   implicit def readable[T](implicit r: Readable[T]): ConfigReadable[T] =
-    string.emap(s => r.read(s))
+    string.emapParsed(s => r.read(s))
 }
