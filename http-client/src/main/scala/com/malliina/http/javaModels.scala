c606918670717980275e032b3367cab70b23c57c
Michael Skogberg <malliina123@gmail.com>
2026-03-07 15:16:35 +0200

    Reuse

--- http-client/src/main/scala/com/malliina/http/javaModels.scala
+++ http-client/src/main/scala/com/malliina/http/javaModels.scala
@@ -1,21 +1,13 @@
 package com.malliina.http
 
-import java.net.http.{HttpResponse => JHttpResponse}
+import java.net.http.{HttpResponse => JHttpResponse, HttpHeaders => JHttpHeaders}
 import java.net.http.HttpResponse.ResponseInfo
 import scala.collection.JavaConverters.{iterableAsScalaIterableConverter, mapAsScalaMapConverter}
+import JavaResponseMeta.toMap
 
 class StringResponse(inner: JHttpResponse[String]) extends HttpResponse {
   override def asString: String = inner.body()
-
-  override def headers: Map[String, Seq[String]] = inner
-    .headers()
-    .map()
-    .asScala
-    .map { case (k, vs) =>
-      k -> vs.asScala.toList
-    }
-    .toMap
-
+  override def headers: Map[String, Seq[String]] = toMap(inner.headers())
   override def code: Int = inner.statusCode()
 }
 
@@ -25,14 +17,17 @@ class JavaBodyResponse(meta: JavaResponseMeta, body: String) extends HttpRespons
   override def code: Int = meta.code
 }
 
-class JavaResponseMeta(inner: ResponseInfo) extends ResponseMeta {
-  override def headers: Map[String, Seq[String]] = inner
-    .headers()
+object JavaResponseMeta {
+  def toMap(hs: JHttpHeaders): Map[String, List[String]] = hs
     .map()
     .asScala
     .map { case (k, vs) =>
       k -> vs.asScala.toList
     }
     .toMap
+}
+
+class JavaResponseMeta(inner: ResponseInfo) extends ResponseMeta {
+  override def headers: Map[String, Seq[String]] = toMap(inner.headers())
   override def code: Int = inner.statusCode()
 }
