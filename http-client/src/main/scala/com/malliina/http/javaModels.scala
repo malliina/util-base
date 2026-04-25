ce57adf2560b4458e3f42fe1dd87d60e1652481f
Michael Skogberg <malliina123@gmail.com>
2026-04-25 18:10:19 +0300

    No internal

--- http-client/src/main/scala/com/malliina/http/javaModels.scala
+++ http-client/src/main/scala/com/malliina/http/javaModels.scala
@@ -4,18 +4,18 @@ import com.malliina.http.JavaResponseMeta.toMap
 
 import java.net.http.{HttpHeaders => JHttpHeaders, HttpResponse => JHttpResponse}
 import java.net.http.HttpResponse.ResponseInfo
-import java.nio.charset.Charset
+import java.nio.charset.{Charset, StandardCharsets}
 import scala.collection.JavaConverters.{iterableAsScalaIterableConverter, mapAsScalaMapConverter}
 
 class JavaResponse(inner: JHttpResponse[Array[Byte]]) extends HttpResponse {
-  override def charset: Charset = jdk.internal.net.http.common.Utils.charsetFrom(inner.headers())
+  override def charset: Charset = StandardCharsets.UTF_8
   override def body: Array[Byte] = inner.body()
   override def headers: Map[String, Seq[String]] = toMap(inner.headers())
   override def code: Int = inner.statusCode()
 }
 
 class JavaResponseInfo(res: ResponseInfo, val body: Array[Byte]) extends HttpResponse {
-  override def charset: Charset = jdk.internal.net.http.common.Utils.charsetFrom(res.headers())
+  override def charset: Charset = StandardCharsets.UTF_8
   override def headers: Map[String, Seq[String]] = toMap(res.headers())
   override def code: Int = res.statusCode()
 }
@@ -31,7 +31,7 @@ object JavaResponseMeta {
 }
 
 class JavaResponseMeta(inner: ResponseInfo) extends ResponseMeta {
-  override def charset: Charset = jdk.internal.net.http.common.Utils.charsetFrom(inner.headers())
+  override def charset: Charset = StandardCharsets.UTF_8
   override def headers: Map[String, Seq[String]] = toMap(inner.headers())
   override def code: Int = inner.statusCode()
 }
